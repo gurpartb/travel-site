1b12861169768e12a33bc71f4012b1ed11c02a44
Gurpartap <Gurpartap.s.bhatti@gmail.com>
2018-06-21 16:24:28 -0700

    Completed large hero block and intro content block.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport, mixins,  cssvars, nested, autoprefixer]))
+        .on('error',function(errorInfo){
+	    console.log(errorInfo.toString());
+	    this.emit('end');
+	})
+	      .pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+    browserSync.init({
+	server: {
+	    baseDir: "app"
+	}
+    });
+    watch('./app/index.html', function(){
+	browserSync.reload();
+    });
+    watch('./app/assets/styles/**/*.css', function(){
+	gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', ['styles'],function(){
+    return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
